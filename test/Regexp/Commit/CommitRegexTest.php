c221fb97506a59f5cf31348e91b004f165af775c
mhavelant <mhavelant@brainsum.com>
2019-01-17 16:50:05 +0100

    BIP-27 | Add tests for checking the commit message regex.
            - Note, the regex is an updated version from PR#8

---
+++ test/Regexp/Commit/CommitRegexTest.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Brainsum\DrupalDevTools\Test\Regexp\Commit;
+
+use GrumPHP\Util\Regex;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test for commit message regular expressions.
+ *
+ * @package Brainsum\DrupalDevToolsTest\Regexp\Commit
+ */
+class CommitRegexTest extends TestCase {
+
+  const REGEX = '/^([A-Z]+-[\d]+ )+\| [A-Za-z\d\s\.]+([^.])+\.{1}$/s';
+
+  /**
+   * Returns valid commit messages.
+   *
+   * @return array
+   *   Array (of arrays) of valid messages.
+   */
+  public function validCommitMessageProvider(): array {
+    return [
+      ['EL-717 | Update Drupal core 8.6.3 to 8.6.5 in Composer lockfile.'],
+      ['CAT-123 | My description.'],
+      ['CAT-112 DOG-323 | 22 items added.'],
+    ];
+  }
+
+  /**
+   * Test valid commit messages.
+   *
+   * @param string $message
+   *   The message.
+   *
+   * @dataProvider validCommitMessageProvider
+   */
+  public function testValidCommitRegex(string $message) {
+    $this->assertTrue($this->regexMatcher($message), "'$message' does not match the regex, when it should.");
+  }
+
+  /**
+   * Returns invalid commit messages.
+   *
+   * @return array
+   *   Array (of arrays) of invalid messages.
+   */
+  public function invalidCommitMessageProvider(): array {
+    return [
+      ['cAt-001 | My description.'],
+      ['CAT-asd | Apple.'],
+      ['CAT-111|description'],
+      ['CAT-112 |description'],
+      ['CAT-113| description'],
+    ];
+  }
+
+  /**
+   * Test invalid commit messages.
+   *
+   * @param string $message
+   *   The message.
+   *
+   * @dataProvider invalidCommitMessageProvider
+   */
+  public function testInvalidCommitRegex($message) {
+    $this->assertNotTrue($this->regexMatcher($message), "'$message' matches the regex, when it should not.");
+  }
+
+  /**
+   * Match a message with the pattern the same way GrumPHP does.
+   *
+   * @param string $message
+   *   The message.
+   *
+   * @return bool
+   *   TRUE, if it's a match, FALSE if it's not.
+   *
+   * @throws \RuntimeException
+   *   When there's a preg_match error.
+   */
+  protected function regexMatcher($message): bool {
+    $regex = new Regex(static::REGEX);
+    // @todo: Read this from the grumphp.yml?
+    $regex->addPatternModifier('m');
+
+    $result = \preg_match((string) $regex, $message);
+    if ($result === FALSE) {
+      throw new \RuntimeException("Error while matching '$message' with pattern '$regex");
+    }
+
+    return (bool) $result;
+  }
+
+}
