cc65003a26529506f64d45d34a7ba9a2d61095b7
mhavelant <mhavelant@brainsum.com>
2019-01-17 16:50:05 +0100

    BIP-27 | Add tests for checking the commit message regex.
            - Note, the regex is an updated version from PR#8

---
+++ test/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * Test bootstrap file.
+ *
+ * Copied from asm89/twig-lint.
+ * @see https://github.com/asm89/twig-lint
+ */
+
+\error_reporting(E_ALL | E_STRICT);
+
+if (\file_exists($file = __DIR__ . '/../vendor/autoload.php')) {
+  $autoload = require_once $file;
+}
+else {
+  throw new \RuntimeException('Install dependencies to run test suite.');
+}
